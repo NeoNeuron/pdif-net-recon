eed0794ad20efdb7c62a094c85057287747c718c
Kai(AMD256) <kchen513@outlook.com>
2025-08-15 19:53:57 +0800

    update GLMCC scripts

--- scripts/benchmark/GLMCC.py
+++ scripts/benchmark/GLMCC.py
@@ -38,13 +38,14 @@ def core_function(key, val, shuffle_id, noise_level=None):
         # val['spk_fname'], sfx=sfx, th=thresholds[key], ref=refs[key])
     conn_fname = val['path'] / val['conn_file']
     conn = np.load(conn_fname)
-    # T = 10
-    T = val['T'] / 1e4
-    if key in ['Gaussian', 'Rcon', 'Logistic', 'RNN']:
-        T /= 10
+    if key in ['HHEE', 'HHEI', 'HHconEE', 'HHconEI']:
+        T = val['T'] / 1e4
+    elif key in ['Gaussian', 'Rcon', 'Logistic', 'RNN', 'Lorenz']:
+        T = val['T'] / 1e6
     W, cpu_time, wall_time = Est_Data(
         val['path'], spk_fname, N=N, T=T, indices=indices[shuffle_id],
-        outfile_sfx=f'{shuffle_id:d}', DELTA=dt[key], WIN=dt[key]*50)
+        outfile_sfx=f'{shuffle_id:d}', DELTA=dt[key], WIN=dt[key]*50,
+        n_jobs=shuffle_id.shape[1])
     try:
         GLMCC = np.load(val['path'] / f"W_GLM_{T:.0f}-{spk_fname:s}_{shuffle_id:d}.npy")
         xx, yy = np.meshgrid(indices[shuffle_id], indices[shuffle_id], indexing='ij')
@@ -62,7 +63,11 @@ def core_function(key, val, shuffle_id, noise_level=None):
 
         recon_df.attrs['wall_time'] = wall_time
         recon_df.attrs['cpu_time']  = cpu_time
-        recon_df.to_pickle(save_path / f'recon_df_noise_0_{key:s}_{shuffle_id:d}.pkl')
+        if noise_level is None:
+            recon_df.to_pickle(save_path / f'recon_df_noise_0_{key:s}_T={T:.0f}_{shuffle_id:d}.pkl')
+        else:
+            recon_df.to_pickle(save_path / f'recon_df_noise_{noise_level:.1f}_{key:s}_T={T:.0f}_{shuffle_id:d}.pkl')
+        # print(recon_df.head(100))
 
     except Exception as e:
         print(e)
